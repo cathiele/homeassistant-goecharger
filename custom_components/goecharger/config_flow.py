5f4b1aa55d42cfa9533ed7230da2e9acf98a8c5a
yannik1015 <34042618+yannik1015@users.noreply.github.com>
2023-07-24 20:36:09 +0200

    Made the options flow display the current values

--- custom_components/goecharger/config_flow.py
+++ custom_components/goecharger/config_flow.py
@@ -57,16 +57,21 @@ class OptionsFlowHandler(config_entries.OptionsFlow):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
+        # Gets current values from config entry
+        current_host = self.config_entry.options.get(CONF_HOST)
+        current_scan_interval = self.config_entry.options.get(CONF_SCAN_INTERVAL)
+        current_correction_factor = self.config_entry.options.get(CONF_CORRECTION_FACTOR)
+
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema(
                 {
-                    vol.Required(CONF_HOST): str,
+                    vol.Required(CONF_HOST, default=current_host): str,
                     vol.Optional(
-                        CONF_SCAN_INTERVAL, default=20
+                        CONF_SCAN_INTERVAL, default=current_scan_interval if current_scan_interval else 20 
                     ): int,
                     vol.Required(
-                        CONF_CORRECTION_FACTOR, default="1.0"
+                        CONF_CORRECTION_FACTOR, default=current_correction_factor if current_correction_factor else "1.0"
                     ): str,
                 }
             )
